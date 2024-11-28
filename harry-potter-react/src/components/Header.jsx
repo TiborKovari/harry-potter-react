7a46b5f971fafc0d3730aa332aa4c414dc0a5502
TiborKovari <142129834+TiborKovari@users.noreply.github.com>
2024-11-29 00:08:16 +0100

    header is optimized to be responsive for all ui sizes

--- harry-potter-react/src/components/Header.jsx
+++ harry-potter-react/src/components/Header.jsx
@@ -1,35 +1,48 @@
+import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 
 function Header() {
   const location = useLocation();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <header className="bg-[#740001] text-gold text-2xl font-serif shadow-md py-4">
-      <nav className="container mx-auto flex justify-between items-center">
-        <h1 className="text-gold text-4xl font-harrypotter tracking-widest">
+    <header className="bg-[#740001] text-gold text-2xl font-serif shadow-md py-4 sticky top-0 z-50">
+      <nav className="container mx-auto flex justify-between items-center px-4">
+        <h1 className="text-gold text-4xl font-harrypotter tracking-widest whitespace-nowrap">
           Wizarding World
         </h1>
-        <ul className="flex space-x-8">
-          {location.pathname !== "/" && (
-            <li className="hover:text-white">
+        <button
+          className="text-gold md:hidden flex flex-col space-y-1 ml-auto"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          <span className="block w-4 h-0.5 bg-gold"></span>
+          <span className="block w-4 h-0.5 bg-gold"></span>
+          <span className="block w-4 h-0.5 bg-gold"></span>
+        </button>
+        <ul className={`${ isMenuOpen ? "block animate-fade-in" : "hidden" 
+        } absolute top-16 left-0 w-full bg-burgundy md:static md:flex md:space-x-8 md:justify-end md:w-auto md:px-4
+        `}>
+            <li 
+            className={`hover:text-white ${
+              location.pathname === "/" ? "bg-gold opacity-70 text-burgundy" : ""
+            }`}>
               <Link to={"/"}>Home</Link>
             </li>
-          )}
-          {location.pathname !== "/houses" && (
-            <li className="hover:text-white">
+            <li className={`hover:text-white ${
+              location.pathname === "/houses" ? "bg-gold opacity-70 text-burgundy" : ""
+            }`}>
               <Link to={"/houses"}>Houses</Link>
             </li>
-          )}
-          {location.pathname !== "/spells" && (
-            <li className="hover:text-white">
+            <li className={`hover:text-white ${
+              location.pathname === "/spells" ? "bg-gold opacity-70 text-burgundy" : ""
+            }`}>
               <Link to={"/spells"}>Spells</Link>
             </li>
-          )}
-          {location.pathname !== "/characters" && (
-            <li className="hover:text-white">
+            <li className={`hover:text-white ${
+              location.pathname === "/characters" ? "bg-gold opacity-70 text-burgundy" : ""
+            }`}>
               <Link to={"/characters"}>Characters</Link>
             </li>
-          )}
         </ul>
       </nav>
     </header>
