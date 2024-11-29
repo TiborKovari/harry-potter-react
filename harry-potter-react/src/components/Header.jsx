17536f5e6e5b14eeb3b6f02b17cab5f40ca54a54
TiborKovari <142129834+TiborKovari@users.noreply.github.com>
2024-11-29 12:27:07 +0100

    refactor header navbarmenu and hamburgerbutton to have it more professional and ux friendly

--- harry-potter-react/src/components/Header.jsx
+++ harry-potter-react/src/components/Header.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import HamburgerButton from "./HamburgerButton";
+import NavbarMenu from "./NavbarMenu";
 
 function Header() {
   const location = useLocation();
@@ -11,39 +13,15 @@ function Header() {
         <h1 className="text-gold text-4xl font-harrypotter tracking-widest whitespace-nowrap">
           Wizarding World
         </h1>
-        <button
-          className="text-gold md:hidden flex flex-col space-y-1 ml-auto"
-          onClick={() => setIsMenuOpen(!isMenuOpen)}
-        >
-          <span className="block w-4 h-0.5 bg-gold"></span>
-          <span className="block w-4 h-0.5 bg-gold"></span>
-          <span className="block w-4 h-0.5 bg-gold"></span>
-        </button>
-        <ul className={`${ isMenuOpen ? "block animate-fade-in" : "hidden" 
-        } absolute top-16 left-0 w-full bg-burgundy md:static md:flex md:space-x-8 md:justify-end md:w-auto md:px-4
-        `}>
-            <li 
-            className={`hover:text-white ${
-              location.pathname === "/" ? "bg-gold opacity-70 text-burgundy" : ""
-            }`}>
-              <Link to={"/"}>Home</Link>
-            </li>
-            <li className={`hover:text-white ${
-              location.pathname === "/houses" ? "bg-gold opacity-70 text-burgundy" : ""
-            }`}>
-              <Link to={"/houses"}>Houses</Link>
-            </li>
-            <li className={`hover:text-white ${
-              location.pathname === "/spells" ? "bg-gold opacity-70 text-burgundy" : ""
-            }`}>
-              <Link to={"/spells"}>Spells</Link>
-            </li>
-            <li className={`hover:text-white ${
-              location.pathname === "/characters" ? "bg-gold opacity-70 text-burgundy" : ""
-            }`}>
-              <Link to={"/characters"}>Characters</Link>
-            </li>
-        </ul>
+        <HamburgerButton
+          isMenuOpen={isMenuOpen}
+          toggleMenu={() => setIsMenuOpen(!isMenuOpen)}
+        />
+        <NavbarMenu
+          isMenuOpen={isMenuOpen}
+          location={location}
+          closeMenu={() => setIsMenuOpen(false)}
+        />
       </nav>
     </header>
   );
